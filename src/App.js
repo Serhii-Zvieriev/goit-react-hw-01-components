079cbd6156c6a0959c5ccc1a78fe70653debbb2c
Serhii-Zvieriev <zacjok@gmail.com>
2021-09-25 13:52:03 +0300

    complite second hw

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import Statistics from './components/Statistics';
 import TransactionHistory from './components/TransactionHistory';
 
 import user from './user.json';
+import statisticalData from './statistical-data.json';
 
 function App() {
   return (
-    <>
+    <div className="container">
       <SocialProfile obj={user} />
+      <Statistics title="Upload stats" stats={statisticalData} />
+      <Statistics stats={statisticalData} />
       <FriendList />
-      <Statistics />
       <TransactionHistory />
-    </>
+    </div>
   );
 }
 
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,8 +1,38 @@
 import PropTypes from 'prop-types';
-import styles from './Statistics.module.css';
+import styles from './Statistics.module.scss';
+import randomColor from 'randomcolor';
 
-function FriendList() {
-  return <div>Statistics</div>;
+function Statistics({ title = '', stats }) {
+  return (
+    <section className={styles.statistics}>
+      {title && <h2 className={styles.title}>{title}</h2>}
+
+      <ul className={styles.statList}>
+        {stats.map(({ id, label, percentage = 0 }) => (
+          <li
+            className={styles.item}
+            key={id}
+            style={{
+              backgroundColor: randomColor(),
+            }}
+          >
+            <span className={styles.label}>{label}</span>
+            <span className={styles.percentage}>{percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
 
-export default FriendList;
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    }),
+  ),
+};
+export default Statistics;
